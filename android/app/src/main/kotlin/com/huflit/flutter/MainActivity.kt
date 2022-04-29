9cdfd56b9eb034ee082e992251c74b5ac4c6ab17
Long Nguyen <nguyenkimlog59@gmail.com>
2022-04-29 12:25:39 +0700

    init

---
+++ android/app/src/main/kotlin/com/huflit/flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.huflit.flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/config/config.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class Config {
+
+  
+  static const appName = 'Fast News';
+  static const supportEmail = 'nguyenkimlog59@gmail.com';
+  static const privacyPolicyUrl = '';
+  static const iOSAppID = '000000';
+
+
+
+  //SOCIAL LINKS
+  static const String facebookPageUrl = 'https://www.facebook.com/longnguyenn59';
+  static const String youtubeChannelUrl = 'https://www.youtube.com/channel/UCDwy1y8-Cg2tw9WNwwcTusA';
+  static const String twitterUrl = 'https://twitter.com';
+  
+
+
+  //app theme color
+  final Color appThemeColor = Color(0xFFf8bd7f);
+
+
+  
+  // Icons
+  static const String appIcon = 'assets/images/icon.png';
+  static const String logo = 'assets/images/logo.png';
+  static const String splash = 'assets/images/splash.png';
+
+  
+
+  //languages
+  static const List<String> languages = [
+    'English',
+    'Spanish',
+    'Arabic'
+  ];
+
+
+
+  //Image Assets
+  static const String commentImage = "assets/images/comment.svg";
+  static const String bookmarkImage = "assets/images/bookmark.svg";
+  static const String notificationImage = "assets/images/notification.svg";
+  static const String noContentImage = "assets/images/no_content.svg";
+
+  
+  
+  //animation files
+  static const String doneAnimation = 'assets/animation_files/done.flr';
+  static const String searchAnimation = 'assets/animation_files/search.flr';
+
+  
+}
