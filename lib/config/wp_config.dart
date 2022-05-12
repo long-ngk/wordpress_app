c985f603119d9a411e6fb6464c4767f191b943bf
Long Nguyen <nguyenkimlog59@gmail.com>
2022-05-12 12:38:05 +0700

    fix: change image thumbnails for categories

--- lib/config/wp_config.dart
+++ lib/config/wp_config.dart
@@ -1,13 +1,10 @@
 import 'package:easy_localization/easy_localization.dart';
 
 class WpConfig {
-  // YOUR WEBISTE URL
   static const String websiteUrl = "http://192.168.1.94:81/wordpress";
 
-  //FEATURE TAG ID
   static const int featuredTagID = 11;
 
-  //VIDEO TAG ID
   static const int videoTagId = 12;
 
   // Hometab 4 categories
@@ -33,7 +30,7 @@ class WpConfig {
   static const String blockedCategoryIds = "";
   static const String blockedCategoryIdsforPopularPosts = "";
 
-  // FEATURE IMGAE -  IF YOUR POSTS DON"T HAVE A FEATURE IMAGE
+  // FEATURE IMAGE -  IF YOUR POSTS DON"T HAVE A FEATURE IMAGE
   static const String randomPostFeatureImage =
       "https://innov8tiv.com/wp-content/uploads/2017/10/blank-screen-google-play-store-1280x720.png";
 
@@ -47,11 +44,15 @@ class WpConfig {
     9: "https://images.unsplash.com/photo-1444653389962-8149286c578a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODV8fGJ1c2luZXNzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
     2: "https://images.unsplash.com/photo-1603739903239-8b6e64c3b185?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGVudGVydGFpbm1lbnR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
     10: "https://images.unsplash.com/photo-1507138086030-616c3b6dd768?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTE1fHxsaWZlc3R5bGV8ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
-    8: "https://images.pexels.com/photos/1250655/pexels-photo-1250655.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    7: "https://images.pexels.com/photos/2698475/pexels-photo-2698475.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    5: "https://images.unsplash.com/photo-1496065187959-7f07b8353c55?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c2NpZW5jZXxlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
     3: "https://images.pexels.com/photos/2885940/pexels-photo-2885940.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    6: "https://images.pexels.com/photos/1036936/pexels-photo-1036936.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
     4: "https://images.pexels.com/photos/3885493/pexels-photo-3885493.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    // science
+    8: "https://images.pexels.com/photos/2166/flight-sky-earth-space.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    // technology
+    7: "https://images.unsplash.com/photo-1550751827-4bd374c3f58b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    // entertainment
+    5: "https://images.unsplash.com/photo-1598899134739-24c46f58b8c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=856&q=80",
+    // travel
+    6: "https://images.pexels.com/photos/2440009/pexels-photo-2440009.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   };
 }
